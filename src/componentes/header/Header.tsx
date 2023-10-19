8dcee64856618349dded5e54a5380f1731ac70c5
Elisamaya2006 <elisacorreia498@gmail.com>
2023-10-19 08:51:53 -0400

    Adicionando formulário "Entre em Contato"

--- src/componentes/header/Header.tsx
+++ src/componentes/header/Header.tsx
@@ -1,31 +1,32 @@
-import "./Header.css";
-function Header() {
-  return (
-    <header>
-      <img src="/elisamaya reis.png" alt="Logo"></img>
-      <div>
-      </div>
-      <nav>
-        <ul>
-          <li>
-            <a href="#">INÍCIO</a>
-          </li>
-          <li>
-            <a href="#">MEMBRO's</a>
-          </li>
-          <li>
-            <a href="#">CONTEÚDO</a>
-          </li>
-          <li>
-            <a href="#">DESIGN</a>
-            </li>
-            <li>
-            <a href="#">CONTATO</a>
-          </li>
-        </ul>
-      </nav>
-      
-      <a href="#" type="cta-button"></a>
+import './Header.css'
+import { Link } from 'react-router-dom';
+function Header(){
+    return(
+        <header>
+                <img src="/elisamaya reis.png" alt="Logo"></img>
+
+            <div>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/">INÍCIO</Link>
+                    </li>
+                    <li>
+                        <Link to="/membro's">MEMBRO'S</Link>
+                    </li>
+                    <li>
+                        <Link to="/conteudo">CONTEÚDO</Link>
+                    </li>
+                    <li>
+                        <Link to="/design">DESIGN</Link>
+                    </li>
+                    <li>
+                        <Link to="/contato">CONTATO</Link>
+                    </li>
+                </ul>
+            </nav>
+            <a href="#" type="cta-button"></a>
       <button className="sticky-button">☰</button>
       <div>
       <a href="https://www.facebook.com/StarCorreia">Facebook </a>
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,64 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import './main.css'
+import Header from './componentes/header/Header'
+import Footer from './componentes/footer/Footer'
+import Main from './componentes/main/Main'
+import Formulario from './componentes/formulario/Formulario'
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: 
+      <>
+        <Header/>
+        <Main/>
+        <Footer/>
+      </>,
+  },
+  {
+    path: "/membro's",
+    element: 
+    <>
+      <Header/>
+      <div className='content-main'>Página Membro's!</div>
+      <Footer/>
+    </>,
+  },
+  {
+    path: "/conteudo",
+    element:
+    <>
+      <Header/>
+      <div className='content-main'>Página de Conteúdo!</div>
+      <Footer/>
+    </>,
+  },
+  {
+    path: "/design",
+    element: 
+      <>
+        <Header/>
+        <div className='content-main'>Página de Design!</div>
+        <Footer/>
+      </>,
+  },
+  {
+    path: "/contato",
+    element: 
+      <>
+        <Header/>
+        <Formulario/>
+        <Footer/>
+      </>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
